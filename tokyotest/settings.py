9ebdd2da07380be1ec0454fabf65209d8585138a
George Rashkov <georg.rashkov@gmail.com>
2021-04-15 13:43:12 +0300

    before update:

--- tokyotest/settings.py
+++ tokyotest/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'tokyotest.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db_2.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
